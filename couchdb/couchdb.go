fa6a6d1bbbde3c955d99d667dad32b7d181838a2
crcrios <crcrios@bancolombia.com.co>
2021-06-29 07:57:16 -0500

    QueryCouchDBWithPagination function is created

--- couchdb/couchdb.go
+++ couchdb/couchdb.go
@@ -19,6 +19,33 @@ func QueryCouchDB(ctx contractapi.TransactionContextInterface, query string) (qu
 	return
 }
 
+// QueryCouchDB lets you execute rich CouchDB queries with pagination
+func QueryCouchDBWithPagination(ctx contractapi.TransactionContextInterface, query string, pageSize int32, bookmark string) (queryResult *bytes.Buffer, err error) {
+	resultsIterator, queryResponseMetadata ,err := ctx.GetStub().GetQueryResultWithPagination(query, pageSize, bookmark)
+	if err != nil {
+		return
+	}
+	defer resultsIterator.Close()
+
+	records, err := constructQueryResponseFromIterator(resultsIterator)
+	if err != nil {
+		return
+	}
+
+	queryResult.WriteString(`{`)
+	queryResult.WriteString(`"records" :`)
+	queryResult.WriteString(records.String())
+	queryResult.WriteString(`,`)
+	queryResult.WriteString(`"bookmark" : "`)
+	queryResult.WriteString(queryResponseMetadata.GetBookmark())
+	queryResult.WriteString(`",`)
+	queryResult.WriteString(`"fetchedRecordsCount" : `)
+	queryResult.WriteString(string(queryResponseMetadata.GetFetchedRecordsCount()))
+	queryResult.WriteString(`}`)
+
+	return
+}
+
 // QueryPrivateData lets you execute rich private data queries
 func QueryPrivateData(ctx contractapi.TransactionContextInterface, collection string, query string) (queryResult *bytes.Buffer, err error) {
 	resultsIterator, err := ctx.GetStub().GetPrivateDataQueryResult(collection, query)
